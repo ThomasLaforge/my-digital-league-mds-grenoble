0dccd9dec2bdc9e8fdebb1c32f270967cc6636b8
Sabrina <sabrinah_38@hotmail.fr>
2026-04-09 15:01:27 +0200

    fix: add missing newline for code readability in event creation page

--- app/organisateur/evenements/creer/page.tsx
+++ app/organisateur/evenements/creer/page.tsx
@@ -1,4 +1,5 @@
 import DynamicCreerGameJamPage from "./DynamicCreationEvent";
+
 export default async function CreerEvenementPage() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/games`);
   let data = await res.json();
