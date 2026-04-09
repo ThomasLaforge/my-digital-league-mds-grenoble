a24ef74f932041a3be6aafe9cff09cdfe1817d5e
Matheo <mat@gmail>
2026-04-09 09:17:26 +0200

    refactor: remplacer @import par @use pour les variables SCSS et nettoyer le code des types

--- app/components/Roadmap/Roadmap.types.ts
+++ app/components/Roadmap/Roadmap.types.ts
@@ -1,11 +1,9 @@
-import { ReactNode } from "react";
-
 export interface RoadmapStep {
   id: string | number;
   title: string;
   description: string;
-  date: string; // "20 Mars 2024" par exemple
+  date: string;
   isCompleted?: boolean;
-  heure?: string; // "18:00" par exemple
+  heure?: string;
   isUpcoming?: boolean;
 }
