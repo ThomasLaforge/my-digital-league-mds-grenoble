5fd306c83b11f847801f2545539c7df8d1c4b8a9
ThomasLaforge <thomas.laforge.38@gmail.com>
2026-04-17 10:59:11 +0200

    Refactor email sending to use Resend service and update Resend package version

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,45 +1,42 @@
-import nodemailer from "nodemailer";
-import { MailtrapTransport } from "mailtrap";
+import { Resend } from "resend";
 
 const domain = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
 
-const transport = nodemailer.createTransport(
-  MailtrapTransport({
-    token: process.env.MAILTRAP_TOKEN!,
-    sandbox: true,
-    testInboxId: Number(process.env.MAILTRAP_INBOX_ID),
-  })
-);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sender = {
-  address: "noreply@mydigitalleague.dev",
-  name: "My Digital League",
-};
+const sender = "noreply@mydigitalleague.dev";
 
-export const sendVerificationEmail = async (
-  email: string,
-  token: string
-) => {
+export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-  await transport.sendMail({
-    from: sender,
-    to: email,
-    subject: "Confirmez votre adresse e-mail",
-    html: `<p>Cliquez <a href="${confirmLink}">ici</a> pour confirmer votre adresse e-mail.</p>`,
-  });
+  try {
+    await resend.emails.send({
+      from: sender,
+      to: email,
+      subject: "Confirmez votre adresse e-mail",
+      html: `<p>Cliquez <a href="${confirmLink}">ici</a> pour confirmer votre adresse e-mail.</p>`,
+    });
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'email de vérification:", error);
+    throw error;
+  }
 };
 
-export const sendPasswordResetEmail = async (
-  email: string,
-  token: string
-) => {
+export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  await transport.sendMail({
-    from: sender,
-    to: email,
-    subject: "Réinitialisation de votre mot de passe",
-    html: `<p>Cliquez <a href="${resetLink}">ici</a> pour réinitialiser votre mot de passe.</p>`,
-  });
+  try {
+    await resend.emails.send({
+      from: sender,
+      to: email,
+      subject: "Réinitialisation de votre mot de passe",
+      html: `<p>Cliquez <a href="${resetLink}">ici</a> pour réinitialiser votre mot de passe.</p>`,
+    });
+  } catch (error) {
+    console.error(
+      "Erreur lors de l'envoi de l'email de réinitialisation:",
+      error
+    );
+    throw error;
+  }
 };
